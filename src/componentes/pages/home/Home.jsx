22ea7e6ad0fcfb86499988f09ce4ead1c0f183e1
santi864 <santiagoandreskerr@gmail.com>
2024-09-10 21:48:35 -0300

    se implementan estados

--- src/componentes/pages/home/Home.jsx
+++ src/componentes/pages/home/Home.jsx
@@ -4,11 +4,13 @@ import "./home.css";
 const Home = () => {
   return (
     <div>
-      <h1>Listado de productos</h1>
-      <ProductCard titulo="Mesa" precio="50000" />
-      <ProductCard titulo="Silla" precio="20000" />
-      <ProductCard titulo="Placard" precio="60000" />
-      <ProductCard titulo="Escritorio" precio="55000" />
+      <h1>Nuestros productos</h1>
+      <ProductCard titulo="Mesa" precio="$50000" />
+      <ProductCard titulo="Silla" precio="$20000" />
+      <ProductCard titulo="Placard" precio="$60000" />
+      <ProductCard titulo="Escritorio" precio="$55000" />
+      <ProductCard titulo="Estanteria" precio="$55000" />
+      <ProductCard titulo="Gazeboo" precio="$150000" />
     </div>
   );
 };
